9033c2ed00c0d878389dd31b846f5d487ae32551
umearesn <akhmetyanov.arslan@yandex.ru>
2020-02-11 18:49:24 +0300

    Initialising of Avalonia project.

---
+++ HomeLibrary_Avalonia/Models/Request/Request.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Network
+{
+    public class Request
+    {
+        private const string basement = "https://core.ac.uk:443/api-v2/search/";
+
+        public string ServerRequest{ get; internal set; }
+
+        //private NameValueCollection queryInfo = HttpUtility.ParseQueryString(string.Empty);
+
+        internal NameValueCollection QueryInfo { get; set; } = HttpUtility.ParseQueryString(string.Empty); 
+
+        public Request()
+        {
+            ServerRequest = basement;
+        }
+
+        public override string ToString()
+        {
+            return $"{ServerRequest}{QueryInfo.ToString()}?apiKey=LY3jJXVTbtixDHlyFoSe14hKs7kNQRAm";
+        }
+    }
+}
---
+++ HomeLibrary_Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeLibrary_Avalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
