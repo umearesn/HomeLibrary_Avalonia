792716757afe1e1d6b653b18c5aaef52294c6528
umearesn <akhmetyanov.arslan@yandex.ru>
2020-04-08 13:47:49 +0300

    Search window state saved.

---
+++ HomeLibrary_Avalonia/Views/SearchWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HomeLibrary_Avalonia.Views
+{
+    public class SearchWindow : Window
+    {
+        public SearchWindow()
+        {
+            //DataContext = new SearchWindow();
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
