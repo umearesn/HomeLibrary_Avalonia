dd59ddbfbf64240937a2a3964e9e89c5b22b1033
umearesn <akhmetyanov.arslan@yandex.ru>
2020-05-17 03:17:13 +0300

    Desing updated.

--- HomeLibrary_Avalonia/Views/HostingView.xaml.cs
+++ HomeLibrary_Avalonia/Views/HostingView.xaml.cs
@@ -1,38 +1,17 @@
-﻿using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+﻿using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using HomeLibrary_Avalonia.Repositories;
 using HomeLibrary_Avalonia.ViewModels;
-using ReactiveUI;
-using System.Reactive.Disposables;
 
 namespace HomeLibrary_Avalonia.Views
 {
     public class HostingView : ReactiveWindow<HostingViewModel>
     {
-
-        private Button goSearch;
-
-        public Button goLocal;
-
         public HostingView()
         {
-
-            goLocal = this.Find<Button>("GoLocal");
-
             ElasticRepository.ConnectRepository();
 
             AvaloniaXamlLoader.Load(this);
-            this.WhenActivated((CompositeDisposable disposable) => {
-                this.OneWayBind(
-                    ViewModel,
-                    vm => vm.IsLocalActivated,
-                    v => v.goLocal.IsEnabled)
-                .DisposeWith(disposable);
-            });
         }
-
-        
-
     }
 }
